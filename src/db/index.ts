b6f09d14b490fd98566ba0dbbc529c4c2f54a154
fixn222 <adeeshakavindu8@gmail.com>
2026-03-18 13:32:51 +0530

    implement database shchemas

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql);
+
+// Export pool as undefined for Serverless to avoid errors in index.ts
+export const pool = undefined;
